e0ef01bed9549fd8f3ca6bb68e81c355ebfad9ee
Hayk <hsimonyan09@gmail.com>
2019-05-31 21:03:14 +0400

    Add routes

---
+++ routes/admin.js
@@ -0,0 +1,50 @@
+const path = require('path');
+
+const express = require('express');
+const { body } = require('express-validator/check');
+
+const adminController = require('../controllers/admin');
+const isAuth = require('../middleware/is-auth');
+
+const router = express.Router();
+
+router.get('/product-list', isAuth, adminController.getProducts);
+
+router.get('/add-product', isAuth, adminController.getAddProduct);
+router.post(
+    '/add-product',
+    [
+      body('title')
+        .isString()
+        .isLength({ min: 3 })
+        .trim(),
+      body('price').isFloat(),
+      body('description')
+        .isLength({ min: 10, max: 400 })
+        .trim()
+    ],
+    isAuth,
+    adminController.postAddProduct
+);
+
+router.get('/edit-product/:productId', isAuth, adminController.getEditProduct);
+
+router.post(
+    '/edit-product/:productId', 
+    [
+      body('title')
+        .isString()
+        .isLength({ min: 3 })
+        .isAlphanumeric()
+        .trim(),
+      body('price').isFloat(),
+      body('description')
+        .isLength({ min: 10, max: 400 })
+        .trim()
+    ],
+    isAuth, 
+    adminController.postEditProduct);
+
+router.post('/delete-product', isAuth, adminController.postDeleteProduct);
+
+module.exports = router;
