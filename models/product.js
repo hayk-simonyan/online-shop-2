be076b994681f254286782c48955597491b6f3c1
Hayk <hsimonyan09@gmail.com>
2019-06-04 18:54:36 +0400

    Add models/order.js,product.js

---
+++ models/product.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  imageUrl: {
+    type: String,
+    required: true
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  }
+});
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;
